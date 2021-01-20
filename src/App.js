bb0f13825b4770445ec5ecf1708f35b25dc47f11
azack2 <azackhussein22@gmail.com>
2021-01-20 14:52:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('Black')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style= {{backgroundColor:color}}
+    
+    >
+
+      
+      <div className='app-footer'>
+        <Footer setColor= {setColor}/>
+
+      </div>
+      <div className= 'app-main'>
+        <Main/>
+        <Sound/>
+
+      </div>
+      <div className= 'app-header'>
+        <Header/>
+
+      </div>
+      <div className= 'app-sound'>
+       
+      </div>
+
+
+    
     </div>
   );
 }
